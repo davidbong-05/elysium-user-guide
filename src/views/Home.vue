5e07e38c843c17195fb8d88baee01dfbc56b84b5
davidbong-05 <davidbong05@gmail.com>
2023-06-17 21:41:20 +0800

    build: 'home' page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <HelloWorld />
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="9">
+        <h1 class="text-center">The Future of Digital Ownership</h1>
+        <p class="my-4 text-center font-weight-bold">
+          Welcome to our NFT marketplace, where digital assets meet blockchain
+          technology to revolutionize ownership and creativity.
+        </p>
+        <p class="text-justify">
+          This project aims to develop an NFT marketplace for Sarawak, with a
+          focus on the art industry. This report included a thorough analysis of
+          user needs and requirements, as well as the design and implementation
+          of the NFT Marketplace. The marketplace allows users to mint, sell,
+          and buy NFTs, bringing potential evolution to the art industry in
+          Sarawak. The project includes the design and development of a working
+          prototype, which allows for minting, selling, and buying of NFTs. The
+          marketplace is expected to be a significant step towards the
+          integration of NFT technology in the art industry in Sarawak and it's
+          expected to be on par with the global NFT marketplaces.
+        </p>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <v-btn
+          color="primary"
+          to="/getting-started"
+          min-width="228"
+          size="x-large"
+          variant="flat"
+        >
+          <v-icon icon="mdi-speedometer-slow" size="large" start />
+          Get Started
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+//
 </script>
