507c533439fda49adaa0675afdca5707d870c29e
davidbong-05 <davidbong05@gmail.com>
2023-08-06 08:13:38 +0800

    feat: added guide for NFT not loading

--- src/views/FAQs.vue
+++ src/views/FAQs.vue
@@ -3,9 +3,8 @@
     <h1>Frequently Asked Questions</h1>
     <v-divider class="my-2"></v-divider>
     <v-card theme="dark" class="px-10 py-2">
-      <v-card-title class="text-h5"  >
-        Why my NFT does not show up on my profile page after I mint/buy
-        it?
+      <v-card-title class="text-h5">
+        Why my NFT does not show up on my profile page after I mint/buy it?
       </v-card-title>
       <v-card-subtitle> There are two cases </v-card-subtitle>
       <v-card-title class="pb-0 mb-0 text-h6">Case 1</v-card-title>
@@ -18,6 +17,26 @@
       <v-card-text>
         You listed the NFT for sale, check out "OnSale" tab inside your profile
         page.
+      </v-card-text>
+    </v-card>
+    <v-divider class="my-2"></v-divider>
+    <v-card theme="dark" class="px-10 py-2">
+      <v-card-title class="text-h5">
+        Why the Top Collections on the homepage does not load?
+      </v-card-title>
+      <v-card-text>
+        Make sure that your MetaMask is properly setup and it is on the Sepolia
+        Test Network. <br />
+        Checkout the
+        <v-btn
+          class="px-1"
+          to="/getting-started"
+          variant="text"
+          color="primary"
+        >
+          Getting Started
+        </v-btn>
+        page for more information.
       </v-card-text>
     </v-card>
     <v-divider class="my-2"></v-divider>
